74e3ba419d3a7a73c80e6d6fa4e3b0fa45d21b52
diego <diegorodrigues-5@hotmail.com>
2022-02-27 18:35:22 -0300

    :recycle: corrigindo alguns arquivos antigos

--- Exercícios Web/node/pastaA/pastaB/tempCodeRunnerFile.js
+++
@@ -1,5 +0,0 @@
-const moduloA = require('../../moduloA')     // ..//..//moduloa   funciona no Mac e Windows mas não no Linux
-console.log(moduloA.ola)
-
-const saudacao = require('./pastaC/index')
-console.log(saudacao.ola2)
